559820438026c44f95bb328c93577dab6de29005
Gautam Rathod <rathodgautam7717@gmail.com>
2023-02-19 14:28:18 +0530

    first commit

---
+++ backend/routes/messageRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { protect } = require('../middlewares/authMiddlewares');
+const {  sendMessage, allMessages } = require('../controllers/messageController');
+
+const router = express.Router();
+
+router.route('/').post(protect, sendMessage)
+
+router.route('/:chatId').get(protect, allMessages)
+
+module.exports = router;
