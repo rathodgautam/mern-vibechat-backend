6cb49eaecc7c7d6c23e35f6898d963a6ed74e7ac
rathodgautam <104826264+rathodgautam@users.noreply.github.com>
2023-02-24 22:33:06 +0530

    cors origin added

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ const PORT = process.env.PORT ;
  const io = require("socket.io")(server, {
     pingTimeout : 60000,
     cors : {
-        origin: "http://localhost:3000",
+        origin: "https://vibechatting.netlify.app/",
     },
  });
 
