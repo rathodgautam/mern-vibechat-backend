3d55d1b2059bf4fb6a19d4e6acac1b1f94d2b93a
rathodgautam <104826264+rathodgautam@users.noreply.github.com>
2023-02-24 22:57:03 +0530

    comment deploment code

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,9 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 
-// app.get("/",(req,res)=>{
-//  res.send( "API is Running");
-// })
+app.get("/",(req,res)=>{
+ res.send( "API is Running");
+})
 
 app.use("/api/user", userRouters);
 app.use("/api/chat", chatRouters);
@@ -25,19 +25,19 @@ app.use("/api/message", messageRouters);
 
 // --------------Deployment-------------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 
 // --------------Deployment-------------
 app.use(notFound);
